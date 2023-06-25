798dbb6be801df7740234d1df6ae51e0a2b73b5f
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-06-25 09:36:51 -0400

    finished assignment

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,12 @@
 function reverseString(str) {
-  // type your code here
+  let reversed_str = ""
+
+  for (let i = str.length -1; i > -1; --i ){
+    reversed_str = reversed_str + str[i];
+  }
+
+  return reversed_str
+
 }
 
 if (require.main === module) {
@@ -15,5 +22,12 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
+
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Create a function called reverseString
+  // Create a variable called reversed_str which is a empty string
+  // iterate through str backwards 
+    // add that iteration to the reverseString
+  // return reversed_str
